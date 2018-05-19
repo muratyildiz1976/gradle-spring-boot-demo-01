0e1abdb0b020bcb9da2ed36f12ef823f11bcd44c
Murat Yildiz <murat_yildiz@gmx.net>
2018-05-19 22:53:23 +0200

    Initial commit.

---
+++ src/main/java/de/murat/tutorial/gradledemo01/GradleDemo01Application.java
@@ -0,0 +1,12 @@
+package de.murat.tutorial.gradledemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleDemo01Application.class, args);
+    }
+}
---
+++ src/test/java/de/murat/tutorial/gradledemo01/GradleDemo01ApplicationTests.java
@@ -0,0 +1,16 @@
+package de.murat.tutorial.gradledemo01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleDemo01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
